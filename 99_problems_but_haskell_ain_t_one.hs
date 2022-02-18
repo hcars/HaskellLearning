a602d375935486b40c658ce0152d184ec1024bc1
hcars <hlc5v@virginia.edu>
2022-02-17 23:36:26 -0500

    Did a few more.

--- 99_problems_but_haskell_ain_t_one.hs
+++ 99_problems_but_haskell_ain_t_one.hs
@@ -1,4 +1,6 @@
 import System.Environment   
+ 
+data NestedList a = Elem a | List [NestedList a]
 
 -- My solutions to https://wiki.haskell.org/99_questions/ 
 
@@ -29,6 +31,35 @@ myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
+-- Problem 6: Check if a palindrome
+-- Not solved myself
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True -- Vacuously true
+isPalindrome (x:[]) = True
+isPalindrome xs = (head xs == myLast xs) && (isPalindrome $ init $ tail xs)
+
+-- Problem 7: Flatten a nested list 
+-- Not solved myself
+myFlatten :: NestedList a ->  [a]
+myFlatten (List []) = []
+myFlatten (Elem a)   = [a]
+myFlatten (List (x:xs)) =   myFlatten x  ++ (myFlatten $ List (xs)) 
+                       
+-- Problem 8: Eliminate consecutive duplicates of list elements. 
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress xs
+ | (head xs) == (head $ tail xs) = compress $ tail xs
+ | otherwise = head xs : (compress $ tail xs)
+
+-- Problem  9: Pack consecutive duplicates of list elements into sublists. 
+-- pack :: (Eq a) => [a] -> [[a]]
+-- pack [] = []
+-- pack [x] = [[x]]
+-- pack xs
+--  | (head xs) == (head $ tail xs) = (head xs) : (pack $ tail xs) : 
+--  | otherwise = head xs : (compress $ tail xs)
 
 main =	do print $ myLast [1 ,2 ,3]
            print $ myLast [1, 2, 3, 4]
@@ -37,4 +68,6 @@ main =	do print $ myLast [1 ,2 ,3]
            print $ myKth "haskell" 5
            print $ myLength ['A'..'Z']
            print $ myReverse ['A' .. 'Z']
-           
+           print $ isPalindrome "tacocat"
+           print $ myFlatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])           
+           print $ compress "aaabbcccxxaaddd"
